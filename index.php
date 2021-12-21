4c96fab7ae7649b239a73d8f3f10cd2178f80711
francescopalmisanodev <francescopalmisano.dev@gmail.com>
2021-12-21 18:01:42 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,80 @@
+<?php
+$database = [
+    [
+        'title' => 'New Jersey',
+        'author' => 'Bon Jovi',
+        'year' => 1988,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg',
+        'genre' => 'Rock'
+    ],
+    [
+        'title' => 'Live at Wembley 86',
+        'author' => 'Queen',
+        'year' => 1992,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg',
+        'genre' => 'Pop'
+    ],
+    [
+        'title' => 'Ten\'s Summoner\'s Tales',
+        'author' => 'Sting',
+        'year' => 1993,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg',
+        'genre' => 'Pop'
+    ],
+    [
+        'title' => 'Steve Gadd band',
+        'author' => 'Steve Gadd Band',
+        'year' => 2018,
+        'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg',
+        'genre' => 'Jazz'
+    ],
+    [
+        'title' => 'And Justice for All',
+        'author' => 'Metallica',
+        'year' => 1988,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81r3FVfNG3L._SY355_.jpg',
+        'genre' => 'Metal'
+    ],
+    [
+        'title' => 'One more car, one more rider',
+        'author' => 'Eric Clapton',
+        'year' => 2002,
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg',
+        'genre' => 'Rock'
+    ]
+];
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Dischi</title>
+</head>
+
+<body>
+    <header>
+        <img src="./logo.png" alt="Spotify">
+    </header>
+    <main>
+        <div class="container">
+            <?php
+            foreach ($database as $song) : ?>
+                <div class="card">
+                    <img src="<?php echo $song["poster"] ?>" alt="<?php echo $song["title"] ?>">
+                    <h2>
+                        <?php echo $song["title"] ?>
+                    </h2>
+                    <h3><?php echo $song["author"] ?></h3>
+                    <h5><?php echo $song["year"] ?></h5>
+                    <h4><?php echo $song["genre"] ?></h4>
+                </div>
+            <?php endforeach ?>
+        </div>
+    </main>
+</body>
+
+</html>
